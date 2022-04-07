26d7f4791eb57bed494e3e34ba07b1602c630c14
Matthew A Elder <matthew@umee.cc>
2022-04-07 10:55:12 -0600

    wip

--- main.go
+++ main.go
@@ -97,29 +97,10 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "!ping" {
-		err := s.MessageReactionAdd(m.ChannelID, m.ID, "👍")
-		if err != nil {
-			log.Error(err)
-			return
-		}
-		err = s.MessageReactionAdd(m.ChannelID, m.ID, "💸")
-		if err != nil {
-			log.Error(err)
-			return
-		}
-		_, err = s.ChannelMessageSend(m.ChannelID, "Pong!")
-		if err != nil {
-			log.Error(err)
-			return
-		}
 	}
 
 	// Do we support this command?
@@ -161,12 +142,8 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				log.Error(err)
 				return
 			}
-			err = s.MessageReactionAdd(m.ChannelID, m.ID, "💸")
-			if err != nil {
-				log.Error(err)
-				return
-			}
-			_, err = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Your address is `%s`; sent coins: %v.", dstAddr, coins))
+			// https://discord.com/developers/docs/resources/channel#mentions
+			_, err = s.ChannelMessageSendReply(m.ChannelID, fmt.Sprintf("Hello <@%s>! Your address is `%s`; I sent you coins: `%s`.", m.Author.ID, dstAddr, coins), m.Reference())
 			if err != nil {
 				log.Error(err)
 			}
